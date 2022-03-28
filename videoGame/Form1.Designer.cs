3abd8334609d3932b44019f478d9c1c1d824f46d
Benjamin Hood <benjhood830@ed.amdsb.ca>
2022-03-28 14:20:46 -0400

    Add project files.

---
+++ videoGame/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace videoGame
+{
+    partial class videoGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.descriptionLabel = new System.Windows.Forms.Label();
+            this.option2Button = new System.Windows.Forms.Button();
+            this.option1Button = new System.Windows.Forms.Button();
+            this.option3Button = new System.Windows.Forms.Button();
+            this.option1Label = new System.Windows.Forms.Label();
+            this.option2Label = new System.Windows.Forms.Label();
+            this.option3Label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Rockwell", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(105, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(614, 77);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "School Simulation ";
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.Lime;
+            this.startButton.Font = new System.Drawing.Font("Rockwell", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(262, 129);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(250, 250);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // descriptionLabel
+            // 
+            this.descriptionLabel.BackColor = System.Drawing.Color.Black;
+            this.descriptionLabel.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.descriptionLabel.ForeColor = System.Drawing.Color.White;
+            this.descriptionLabel.Location = new System.Drawing.Point(89, 9);
+            this.descriptionLabel.Name = "descriptionLabel";
+            this.descriptionLabel.Size = new System.Drawing.Size(621, 107);
+            this.descriptionLabel.TabIndex = 2;
+            // 
+            // option2Button
+            // 
+            this.option2Button.Location = new System.Drawing.Point(140, 312);
+            this.option2Button.Name = "option2Button";
+            this.option2Button.Size = new System.Drawing.Size(54, 52);
+            this.option2Button.TabIndex = 3;
+            this.option2Button.UseVisualStyleBackColor = true;
+            this.option2Button.Visible = false;
+            this.option2Button.Click += new System.EventHandler(this.option2Button_Click);
+            // 
+            // option1Button
+            // 
+            this.option1Button.Location = new System.Drawing.Point(140, 234);
+            this.option1Button.Name = "option1Button";
+            this.option1Button.Size = new System.Drawing.Size(54, 52);
+            this.option1Button.TabIndex = 4;
+            this.option1Button.UseVisualStyleBackColor = true;
+            this.option1Button.Visible = false;
+            this.option1Button.Click += new System.EventHandler(this.option1Button_Click);
+            // 
+            // option3Button
+            // 
+            this.option3Button.Location = new System.Drawing.Point(140, 386);
+            this.option3Button.Name = "option3Button";
+            this.option3Button.Size = new System.Drawing.Size(54, 52);
+            this.option3Button.TabIndex = 5;
+            this.option3Button.UseVisualStyleBackColor = true;
+            this.option3Button.Visible = false;
+            this.option3Button.Click += new System.EventHandler(this.option3Button_Click);
+            // 
+            // option1Label
+            // 
+            this.option1Label.BackColor = System.Drawing.Color.Black;
+            this.option1Label.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option1Label.ForeColor = System.Drawing.Color.White;
+            this.option1Label.Location = new System.Drawing.Point(200, 248);
+            this.option1Label.Name = "option1Label";
+            this.option1Label.Size = new System.Drawing.Size(581, 52);
+            this.option1Label.TabIndex = 6;
+            // 
+            // option2Label
+            // 
+            this.option2Label.BackColor = System.Drawing.Color.Black;
+            this.option2Label.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option2Label.ForeColor = System.Drawing.Color.White;
+            this.option2Label.Location = new System.Drawing.Point(200, 327);
+            this.option2Label.Name = "option2Label";
+            this.option2Label.Size = new System.Drawing.Size(581, 52);
+            this.option2Label.TabIndex = 7;
+            // 
+            // option3Label
+            // 
+            this.option3Label.BackColor = System.Drawing.Color.Black;
+            this.option3Label.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option3Label.ForeColor = System.Drawing.Color.White;
+            this.option3Label.Location = new System.Drawing.Point(200, 400);
+            this.option3Label.Name = "option3Label";
+            this.option3Label.Size = new System.Drawing.Size(581, 52);
+            this.option3Label.TabIndex = 8;
+            // 
+            // videoGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.option3Button);
+            this.Controls.Add(this.option1Button);
+            this.Controls.Add(this.option2Button);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.descriptionLabel);
+            this.Controls.Add(this.option1Label);
+            this.Controls.Add(this.option2Label);
+            this.Controls.Add(this.option3Label);
+            this.Name = "videoGame";
+            this.Text = "School Day";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label descriptionLabel;
+        private System.Windows.Forms.Button option2Button;
+        private System.Windows.Forms.Button option1Button;
+        private System.Windows.Forms.Button option3Button;
+        private System.Windows.Forms.Label option1Label;
+        private System.Windows.Forms.Label option2Label;
+        private System.Windows.Forms.Label option3Label;
+    }
+}
+
---
+++ videoGame/Form1.cs
@@ -0,0 +1,420 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// "descriptionLabel" is the name od the label to ouput the current situation
+//option 1 = red
+// option 2 = blue
+namespace videoGame
+{
+    public partial class videoGame : Form
+    {
+        int page = 1;
+        Random randGen = new Random();   
+        public videoGame()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            page = 1;
+            DisplayPage();
+        }
+
+        private void option1Button_Click(object sender, EventArgs e)
+        {
+            /// Check what page we are currently on, and then flip
+            /// to the page you need to go to if you selected option 1
+
+            if (page == 1)
+            {
+                page = 2;
+            }
+            else if (page == 2)
+            {
+                page = 3;
+            }
+            else if (page == 3)
+            {
+                page = 6;
+            }
+            else if (page == 6)
+            {
+                int randValue = randGen.Next(1, 101);
+
+                if(randValue < 99)
+                {
+                    page = 7;
+                }
+                else
+                {
+                    page = 96;
+                }
+            }
+            else if (page ==4)
+            {
+                page = 6;
+            }
+            else if (page == 26)
+            {
+                page = 96;
+            }
+            else if (page == 39)
+            {
+                page = 11;
+            }
+            else if (page == 9)
+            {
+                int randValue = randGen.Next(1, 101);
+
+                if(randValue > 50)
+                {
+                    page = 10;
+                }
+                else
+                {
+                    page = 96;
+                }
+            }
+            else if (page == 13)
+            {
+                page = 25;
+            }
+            else if (page == 16)
+            {
+                int randValue = randGen.Next(1, 101);
+
+                if (randValue < 60)
+                {
+                    page = 17;
+                }
+                else
+                {
+                    page = 18;
+                }
+            }
+            else if (page == 19)
+            {
+                int randValue = randGen.Next(1, 101);
+
+                if(randValue < 90)
+                {
+                    page = 96;
+                }
+                else
+                {
+                    page = 99;
+                }
+            }
+            else if (page == 20)
+            {
+                int randValue = randGen.Next(1, 101);
+                if (randValue > 90)
+                {
+                    page = 99;
+                }
+                else
+                {
+                    page = 96;
+                }
+            }
+            else if (page == 29)
+            {
+                page = 96;
+            }
+            else if (page == 96)
+            {
+                page = 98;
+            }
+            DisplayPage();
+        }
+
+        private void option2Button_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 29;
+            }
+            else if (page == 29)
+            {
+                page = 96;
+            }
+            else if (page == 2)
+            {
+                page = 5;
+            }
+            else if (page == 26)
+            {
+                page = 96;
+            }
+            else if (page == 4)
+            {
+                page = 39;
+            }
+            else if (page == 39)
+            {
+                page = 9;
+            }
+            else if (page == 13)
+            {
+                page = 14;
+            }
+            else if (page == 15)
+            {
+                page = 16;
+            }
+            else if (page == 19)
+            {
+                page = 21;
+            }
+            DisplayPage();
+        }
+        public void DisplayPage()
+        {this.Text = page.ToString();
+
+            switch(page)
+            {
+                case 1:
+                    titleLabel.Text = "";
+                    startButton.Visible = false;
+                    option1Button.Visible = true;
+                    option2Button.Visible = true;
+                    descriptionLabel.Text = "It's another beautiful morning as the warmth of June seeps through the window.";
+                    option1Label.Text = "Keep Sleeping";
+                    option2Label.Text = "Wake up";
+                    break;
+                case 2:
+                    descriptionLabel.Text = "It's now 10 am and your mother comes in FURIOUS.";
+                    option1Label.Text = "Punch your mother";
+                    option2Label.Text = "Run away";
+                    break;
+                case 3:
+                    descriptionLabel.Text = "Your mother retaliates with sucker punching you in the face.";
+                    option3Label.Text = "Continue";
+                    option2Label.Text = "";
+                    option1Label.Text = "";
+
+                    option3Button.Visible = true;
+                    option1Button.Visible = false;
+                    option2Button.Visible = false;
+                    break;
+                case 4:
+                    descriptionLabel.Text = "Your father grabs you off the floor and wants to brings you to the hospital. ";
+                    option1Label.Text = "Go with him";
+                    option2Label.Text = "Don't go with him";
+                    option3Label.Text = "";
+
+                    option3Button.Visible = false;
+                    option1Button.Visible = true;
+                    option2Button.Visible = true;
+                    break;
+                case 5:
+                    descriptionLabel.Text = "You finally reach an alley to hide in. You see a strange looking man in the alley hiding behind the dumpster. You decide to ignore him and run the other way down the alley.";
+                    option3Label.Text = "Continue";
+                    option2Label.Text = "";
+                    option1Label.Text = "";
+
+                    option3Button.Visible = true;
+                    option1Button.Visible = false;
+                    option2Button.Visible = false;
+                    break;
+                case 6:
+                    descriptionLabel.Text = "While arguing with you, your father hits a bump in the road and the car slides off the road into the gutter.";
+                    option3Label.Text = "";
+                    option2Label.Text = "";
+                    option1Label.Text = "Continue";
+
+                    option3Button.Visible = false;
+                    option1Button.Visible = true;
+                    option2Button.Visible = false;
+                    break;
+                case 7:
+                    descriptionLabel.Text = "You leave your father for dead in the car wreck and walk to the hospital.";
+                    option3Label.Text = "Continue";
+                    option2Label.Text = "";
+                    option1Label.Text = "";
+
+                    option3Button.Visible = true;
+                    option1Button.Visible = false;
+                    option2Button.Visible = false;
+                    break;
+                case 8:
+                    descriptionLabel.Text = "Your mother forgives you and you get the rest of the day off school.";
+                    option3Label.Text = "Continue";
+                    option2Label.Text = "";
+                    option1Label.Text = "";
+
+                    option3Button.Visible = true;
+                    option1Button.Visible = false;
+                    option2Button.Visible = false;
+                    break;
+                case 9:
+                    descriptionLabel.Text = "There's a man hiding behind a dumpster in the alley. He pops out and takes a swing at you with a baseball bat.";
+                    option3Label.Text = "";
+                    option2Label.Text = "";
+                    option1Label.Text = "Coninue";
+
+                    option3Button.Visible = false;
+                    option1Button.Visible = true;
+                    option2Button.Visible = false;
+                    break;
+                case 10:
+                    descriptionLabel.Text = "You barely make it out alive but manage to run back the way you came from and into the haunted house.";
+                    option3Label.Text = "Continue";
+                    option3Button.Visible = true;
+                    break;
+                case 11:
+                    descriptionLabel.Text = "You try to enter the haunted house but the door is locked. You decide to leave and go back to your house.";
+                    option3Label.Text = "Continue";
+                    option3Button.Visible = true;
+                    break;
+                case 12:
+                    descriptionLabel.Text = "You try to enter the haunted house but the door is locked. You decide to leave and go back to your house.";
+                    option3Label.Text = "Continue";
+                    option3Button.Visible = true;
+                    break;
+                case 13:
+                    descriptionLabel.Text = "You can decide to leave them there to die, or put them in the car and take them to the hospital.";
+                    option1Label.Text = "Take Them";
+                    option2Label.Text = "Leave Them";
+                    option3Label.Text = "";
+
+                    option1Button.Visible = true;
+                    option2Button.Visible = true;
+                    option3Button.Visible = false;
+                    break;
+                case 14:
+                    descriptionLabel.Text = "10 years later, you run out of money going to college and are forced to play guitar on the streets for money.";
+                    option1Label.Text = "";
+                    option2Label.Text = "";
+                    option3Label.Text = "Continue";
+
+                    option1Button.Visible = false;
+                    option2Button.Visible = false;
+                    option3Button.Visible = true;
+                    break;
+                case 15:
+                    descriptionLabel.Text = "You begin to gain popularity of a couple of years playing. RBMG records discovers and online video of you playing and shows interest in you.";
+                    option1Label.Text = "Sign With Them";
+                    option2Label.Text = "";
+                    option3Label.Text = "";
+
+                    option1Button.Visible = true;
+                    option2Button.Visible = false;
+                    option3Button.Visible = false;
+                    break;
+                case 16:
+                    descriptionLabel.Text = "You and RBMG release your debut album.";
+                    option1Label.Text = "Continue";
+                    option2Label.Text = "";
+                    option3Label.Text = "";
+
+                    option1Button.Visible = true;
+                    option2Button.Visible = false;
+                    option3Button.Visible = false;
+                    break;
+                case 17:
+                    descriptionLabel.Text = "You continue your guitar playing on the streets for another 2 years. You finally save enough money to afford your own recording session.";
+                    option1Label.Text = "";
+                    option2Label.Text = "";
+                    option3Label.Text = "Continue";
+
+                    option1Button.Visible = false;
+                    option2Button.Visible = false;
+                    option3Button.Visible = true;
+                    break;
+                case 18:
+                    descriptionLabel.Text = "Your song/songs went viral and you earn 1 million dollars and are able to purchase a house in Los Angeles.";
+                    option1Label.Text = "";
+                    option2Label.Text = "";
+                    option3Label.Text = "Continue";
+
+                    option1Button.Visible = false;
+                    option2Button.Visible = false;
+                    option3Button.Visible = true;
+                    break;
+                case 19:
+                    descriptionLabel.Text = "After finished recording, you decide to publish your song.";
+                    option1Label.Text = "Continue";
+                    option2Label.Text = "";
+                    option3Label.Text = "";
+
+                    option1Button.Visible = true;
+                    option2Button.Visible = false;
+                    option3Button.Visible = false;
+                    break;
+                case 20:
+                    descriptionLabel.Text = "With your remaining $50,000 you decide to gamble it all at a casino.";
+                    option1Label.Text = "Continue";
+                    option2Label.Text = "";
+                    option3Label.Text = "";
+
+                    option1Button.Visible = true;
+                    option2Button.Visible = false;
+                    option3Button.Visible = false;
+                    break;
+                case 21:
+                    descriptionLabel.Text = "You lose all your money and overdose 10 days later.";
+                    option1Label.Text = "";
+                    option2Label.Text = "";
+                    option3Label.Text = "Continue";
+
+                    option1Button.Visible = false;
+                    option2Button.Visible = false;
+                    option3Button.Visible = true;
+                    break;
+            }
+
+
+        }
+
+        private void option3Button_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 96;
+            }
+            else if (page == 3) 
+            {
+                page = 4;
+            }
+            else if (page == 5) 
+            {
+                page = 26;
+            }
+            else if (page == 7)
+            {
+                page = 8;
+            }
+            else if (page == 8)
+            {
+                page = 99;
+            }
+            else if (page == 10)
+            {
+                page = 11;
+            }
+            else if (page == 17)
+            {
+                page = 19;
+            }
+            else if (page ==18)
+            {
+                page = 20;
+            }
+            else if (page == 19)
+            {
+                page = 96;
+            }
+            DisplayPage();
+        }
+       
+    }
+}
